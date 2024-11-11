6c78c76c8c4617e4999bbb31033d50883521eaf9
gulzaib002 <gulzaib.dev@gmail.com>
2024-11-11 15:32:55 +0500

    wellcome banner

--- home.php
+++ home.php
@@ -14,7 +14,7 @@ get_header(); // Load the header
 
 <main id="content" class="kazflix-home-main">
     <!-- Main Content Area with Post Loop -->
-    <?php get_template_part( 'template-parts/kazflix-top-banner', 'home' ); ?>
+    <?php get_template_part( 'template-parts/banners/kazflix-wellcome-banner', 'home' ); ?>
 
     <?php get_template_part( 'template-parts/kazflix-slider-latest-posts', 'home' ); ?>
 
