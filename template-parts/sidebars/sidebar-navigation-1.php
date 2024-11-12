a6b0daa63b4ea7ca55b7ec404f6cab9b6e336022
gulzaib002 <gulzaib.dev@gmail.com>
2024-11-12 10:54:12 +0500

    mobile sidebar

--- template-parts/sidebars/sidebar-navigation-1.php
+++ template-parts/sidebars/sidebar-navigation-1.php
@@ -11,31 +11,40 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <aside class="sidebar-navigation">
+    <!-- Mobile Search and Back Button -->
+    <div class="search-container">
+        <button class="back-button"><i class="icon-back"></i> <span>Back</span></button>
+        <div class="search-bar">
+            <i class="icon-search"></i>
+            <input type="text" placeholder="Search">
+        </div>
+    </div>
+
     <nav>
         <ul class="sidebar-menu">
-            <li><a href="#"><i class="icon-home"></i> Home</a></li>
-            <li><a href="#"><i class="icon-recent"></i> Recently played</a></li>
-            <li><a href="#"><i class="icon-new"></i> New</a></li>
-            <li><a href="#"><i class="icon-trending"></i> Trending now</a></li>
-            <li><a href="#"><i class="icon-updated"></i> Updated</a></li>
-            <li><a href="#"><i class="icon-originals"></i> Originals</a></li>
+            <li><a href="#"><i class="icon-home"></i> <span>Home</span></a></li>
+            <li><a href="#"><i class="icon-recent"></i> <span>Recently played</span></a></li>
+            <li><a href="#"><i class="icon-new"></i> <span>New</span></a></li>
+            <li><a href="#"><i class="icon-trending"></i> <span>Trending now</span></a></li>
+            <li><a href="#"><i class="icon-updated"></i> <span>Updated</span></a></li>
+            <li><a href="#"><i class="icon-originals"></i> <span>Originals</span></a></li>
             <hr>
-            <li><a href="#"><i class="icon-two-player"></i> 2 Player</a></li>
-            <li><a href="#"><i class="icon-action"></i> Action</a></li>
-            <li><a href="#"><i class="icon-adventure"></i> Adventure</a></li>
-            <li><a href="#"><i class="icon-basketball"></i> Basketball</a></li>
-            <li><a href="#"><i class="icon-beauty"></i> Beauty</a></li>
-            <li><a href="#"><i class="icon-bike"></i> Bike</a></li>
-            <li><a href="#"><i class="icon-car"></i> Car</a></li>
-            <li><a href="#"><i class="icon-card"></i> Card</a></li>
-            <li><a href="#"><i class="icon-casual"></i> Casual</a></li>
-            <li><a href="#"><i class="icon-clicker"></i> Clicker</a></li>
-            <li><a href="#"><i class="icon-controller"></i> Controller</a></li>
-            <li><a href="#"><i class="icon-dress-up"></i> Dress Up</a></li>
-            <li><a href="#"><i class="icon-driving"></i> Driving</a></li>
-            <li><a href="#"><i class="icon-escape"></i> Escape</a></li>
-            <li><a href="#"><i class="icon-flash"></i> Flash</a></li>
-            <li><a href="#"><i class="icon-fps"></i> FPS</a></li>
+            <li><a href="#"><i class="icon-two-player"></i> <span>2 Player</span></a></li>
+            <li><a href="#"><i class="icon-action"></i> <span>Action</span></a></li>
+            <li><a href="#"><i class="icon-adventure"></i> <span>Adventure</span></a></li>
+            <li><a href="#"><i class="icon-basketball"></i> <span>Basketball</span></a></li>
+            <li><a href="#"><i class="icon-beauty"></i> <span>Beauty</span></a></li>
+            <li><a href="#"><i class="icon-bike"></i> <span>Bike</span></a></li>
+            <li><a href="#"><i class="icon-car"></i> <span>Car</span></a></li>
+            <li><a href="#"><i class="icon-card"></i> <span>Card</span></a></li>
+            <li><a href="#"><i class="icon-casual"></i> <span>Casual</span></a></li>
+            <li><a href="#"><i class="icon-clicker"></i> <span>Clicker</span></a></li>
+            <li><a href="#"><i class="icon-controller"></i> <span>Controller</span></a></li>
+            <li><a href="#"><i class="icon-dress-up"></i> <span>Dress Up</span></a></li>
+            <li><a href="#"><i class="icon-driving"></i> <span>Driving</span></a></li>
+            <li><a href="#"><i class="icon-escape"></i> <span>Escape</span></a></li>
+            <li><a href="#"><i class="icon-flash"></i> <span>Flash</span></a></li>
+            <li><a href="#"><i class="icon-fps"></i> <span>FPS</span></a></li>
         </ul>
     </nav>
 </aside>
