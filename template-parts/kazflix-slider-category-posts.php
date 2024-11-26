9f239866fc5e54e12524aef98006eb88062a3705
gulzaib002 <gulzaib.dev@gmail.com>
2024-11-26 23:41:49 +0500

    added navigation buttons

--- template-parts/kazflix-slider-category-posts.php
+++ template-parts/kazflix-slider-category-posts.php
@@ -31,21 +31,23 @@ if ( ! empty( $categories ) ) :
                     <?php esc_html_e( 'View More', 'kazflix' ); ?>
                 </a>
             </div>
-            <div class="kazflix-scroll-wrapper">
-                <!-- <button class="kazflix-scroll-button left-button"  id="kazflix-scroll-left">‹</button> -->
-                <div class="kazflix-mixed-layout-horizontal">
-                    <?php
-                    // Check if it's the second category
-                    $container_class = ($category_count === 2) ? 'kazflix-post-card-container-category-short' : 'kazflix-post-card-container-category';
-                    
-                    while ( $query->have_posts() ) : $query->the_post(); ?>
-                        <!-- Single Post Card with conditional class based on category count -->
-                        <div class="<?php echo $container_class; ?>">
-                            <?php get_template_part( 'template-parts/post-cards/content', 'post-card-1' ); ?>
-                        </div>
-                    <?php endwhile; ?>
+            <div class="kazflix-carousel-container">
+                <div class="kazflix-scroll-wrapper">
+                    <button class="kazflix-scroll-button left-button"  id="kazflix-scroll-left">‹</button>
+                    <div class="kazflix-mixed-layout-horizontal">
+                        <?php
+                        // Check if it's the second category
+                        $container_class = ($category_count === 2) ? 'kazflix-post-card-container-category-short' : 'kazflix-post-card-container-category';
+                        
+                        while ( $query->have_posts() ) : $query->the_post(); ?>
+                            <!-- Single Post Card with conditional class based on category count -->
+                            <div class="<?php echo $container_class; ?>">
+                                <?php get_template_part( 'template-parts/post-cards/content', 'post-card-1' ); ?>
+                            </div>
+                        <?php endwhile; ?>
+                    </div>
+                    <button class="kazflix-scroll-button right-button" id="kazflix-scroll-right">›</button>
                 </div>
-                <!-- <button class="kazflix-scroll-button right-button" id="kazflix-scroll-right">›</button> -->
             </div>
             <?php wp_reset_postdata(); ?>
 
